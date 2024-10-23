35c3f97aebc39524d53e93ca8321e1f355d44b4a
thekaroe-thailand <thekaroe@gmail.com>
2024-10-23 20:51:34 +0700

    billInvoice

--- controllers/SaleTempController.js
+++ controllers/SaleTempController.js
@@ -438,7 +438,205 @@ module.exports = {
             return res.send({ message: 'success', fileName: fileName });
         } catch (e) {
             return res.status(500).send({ error: e.message })
-
         }
-    }
+    },
+    endSale: async (req, res) => {
+        try {
+            const saleTemps = await prisma.saleTemp.findMany({
+                include: {
+                    SaleTempDetails: {
+                        include: {
+                            Food: true
+                        }
+                    }
+                },
+                where: {
+                    userId: req.body.userId
+                }
+            });
+
+            const billSale = await prisma.billSale.create({
+                data: {
+                    amount: req.body.amount,
+                    inputMoney: req.body.inputMoney,
+                    payType: req.body.payType,
+                    tableNo: req.body.tableNo,
+                    userId: req.body.userId,
+                    returnMoney: req.body.returnMoney
+                }
+            })
+
+            for (let i = 0; i < saleTemps.length; i++) {
+                const item = saleTemps[i];
+
+                if (item.SaleTempDetails.length > 0) {
+                    // have details
+                    for (let j = 0; j < item.SaleTempDetails.length; j++) {
+                        const detail = item.SaleTempDetails[j];
+                        await prisma.billSaleDetail.create({
+                            data: {
+                                billSaleId: billSale.id,
+                                foodId: detail.foodId,
+                                price: detail.Food.price,
+                                moneyAdded: detail.moneyAdded,
+                                tasteId: detail.tasteId
+                            }
+                        })
+                    }
+                } else {
+                    if (item.qty > 0) {
+                        // qty > 0
+                        for (let j = 0; j < item.qty; j++) {
+                            await prisma.billSaleDetail.create({
+                                data: {
+                                    billSaleId: billSale.id,
+                                    foodId: item.foodId,
+                                    price: item.Food.price
+                                }
+                            })
+                        }
+                    } else {
+                        // qty = 0
+                        await prisma.billSaleDetail.create({
+                            data: {
+                                billSaleId: billSale.id,
+                                foodId: item.foodId,
+                                price: item.Food.price
+                            }
+                        })
+                    }
+                }
+            }
+
+            // 
+            // clear sale temp and detail
+            //
+            for (let i = 0; i < saleTemps.length; i++) {
+                const item = saleTemps[i];
+                await prisma.saleTempDetail.deleteMany({
+                    where: {
+                        saleTempId: item.id
+                    }
+                })
+            }
+
+            // delete saleTemp
+            await prisma.saleTemp.deleteMany({
+                where: {
+                    userId: req.body.userId
+                }
+            })
+
+            return res.send({ message: 'success' })
+        } catch (e) {
+            return res.status(500).send({ error: e.message })
+        }
+    },
+    printBillAfterPay: async (req, res) => {
+        try {
+            // organization
+            const organization = await prisma.organization.findFirst();
+
+            // rows in saleTemps
+            const billSale = await prisma.billSale.findFirst({
+                include: {
+                    BillSaleDetails: {
+                        include: {
+                            Food: true
+                        }
+                    },
+                    User: true
+                },
+                where: {
+                    userId: req.body.userId,
+                    tableNo: req.body.tableNo,
+                    status: 'use'
+                }
+            });
+
+            const BillSaleDetails = billSale.BillSaleDetails;
+
+            // create bill by pkfkit
+            const pdfkit = require('pdfkit');
+            const fs = require('fs');
+            const dayjs = require('dayjs');
+
+            const paperWidth = 80;
+            const padding = 3;
+
+            const doc = new pdfkit({
+                size: [paperWidth, 200],
+                margins: {
+                    top: 3,
+                    bottom: 3,
+                    left: 3,
+                    right: 3,
+                },
+            });
+            const fileName = `uploads/invoice-${dayjs(new Date()).format('YYYYMMDDHHmmss')}.pdf`;
+            const font = 'Kanit/kanit-regular.ttf';
+
+            doc.pipe(fs.createWriteStream(fileName));
+
+            // display logo
+            const imageWidth = 20;
+            const positionX = (paperWidth / 2) - (imageWidth / 2);
+            doc.image('uploads/' + organization.logo, positionX, 5, {
+                align: 'center',
+                width: imageWidth,
+                height: 20
+            })
+            doc.moveDown();
+
+            doc.font(font);
+            doc.fontSize(5).text('*** ใบเสร็จรับเงิน ***', 20, doc.y + 8);
+            doc.fontSize(8);
+            doc.text(organization.name, padding, doc.y);
+            doc.fontSize(5);
+            doc.text(organization.address);
+            doc.text(`เบอร์โทร: ${organization.phone}`);
+            doc.text(`เลขประจำตัวผู้เสียภาษี: ${organization.taxCode}`);
+            doc.text(`โต๊ะ: ${req.body.tableNo}`, { align: 'center' });
+            doc.text(`วันที่: ${dayjs(new Date()).format('DD/MM/YYYY HH:mm:ss')}`, { align: 'center' });
+            doc.text('รายการอาหาร', { align: 'center' });
+            doc.moveDown();
+
+            const y = doc.y;
+            doc.fontSize(4);
+            doc.text('รายการ', padding, y);
+            doc.text('ราคา', padding + 18, y, { align: 'right', width: 20 });
+            doc.text('จำนวน', padding + 36, y, { align: 'right', width: 20 });
+            doc.text('รวม', padding + 55, y, { align: 'right' });
+
+            // line
+            // set border height
+            doc.lineWidth(0.1);
+            doc.moveTo(padding, y + 6).lineTo(paperWidth - padding, y + 6).stroke();
+
+            // loop saleTemps
+            BillSaleDetails.map((item, index) => {
+                const y = doc.y;
+                doc.text(item.Food.name, padding, y);
+                doc.text(item.Food.price, padding + 18, y, { align: 'right', width: 20 });
+                doc.text(1, padding + 36, y, { align: 'right', width: 20 });
+                doc.text(item.Food.price * 1, padding + 55, y, { align: 'right' });
+            });
+
+            // sum amount
+            let sumAmount = 0;
+            saleTemps.forEach((item) => {
+                sumAmount += item.Food.price * 1;
+            });
+
+            // display amount
+            doc.text(`รวม: ${sumAmount.toLocaleString('th-TH')} บาท`, padding, doc.y, { align: 'right', width: paperWidth - padding - padding });
+            doc.text('รับเงิน ' + billSale.inputMoney.toLocaleString('th-TH') + ' บาท', padding, doc.y, { align: 'right', width: paperWidth - padding - padding });
+            doc.text('เงินทอน ' + billSale.returnMoney.toLocaleString('th-TH') + ' บาท', padding, doc.y, { align: 'right', width: paperWidth - padding - padding });
+            doc.end();
+
+            return res.send({ message: 'success', fileName: fileName });
+        } catch (e) {
+            return res.status(500).send({ error: e.message })
+        }
+    },
 }
