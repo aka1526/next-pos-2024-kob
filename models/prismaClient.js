c7bf91be4e6397ce5f8b1cc47b8b75a5335b8f2d
Akachai Pijan <akachai1526@hotmail.com>
2024-11-01 14:54:35 +0700

    Add new file

--- models/prismaClient.js
+++ models/prismaClient.js
@@ -2,8 +2,8 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient({
     log: [
       { level: 'query', emit: 'event' }, // Log queries
-      { level: 'info', emit: 'event' },  // Log info messages
-      { level: 'warn', emit: 'event' },  // Log warnings
+      // { level: 'info', emit: 'event' },  // Log info messages
+      // { level: 'warn', emit: 'event' },  // Log warnings
       { level: 'error', emit: 'event' }, // Log errors
     ],
   });
@@ -11,7 +11,7 @@ const prisma = new PrismaClient({
     console.log('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'  );
     console.log('Query: ' + e.query);
     console.log('Params: ' + e.params);
-    console.log('Duration: ' + e.duration + 'ms');
+    //console.log('Duration: ' + e.duration + 'ms');
     console.log('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'  );
   });
 module.exports = prisma;
